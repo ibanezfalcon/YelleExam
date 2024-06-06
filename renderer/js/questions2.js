4ebb7652c9974dd3ef2fe861a9bd861eac3a0a27
ibanezfalconf <ibanezfalconf@gmail.com>
2024-06-07 01:21:35 +0800

    changes11 commit

--- renderer/js/questions2.js
+++ renderer/js/questions2.js
@@ -1,6 +1,6 @@
 const questions2 = [
     {
-        image: "image1.png",
+        image: "image1.PNG",
         questions: [
             {
                 question: "What is the architectural style of figure A?",
@@ -54,59 +54,59 @@ const questions2 = [
             }
         ]
     },
-    // {
-    //     image: "image2.png",
-    //     questions: [
-    //         {
-    //             question: "Item A is called _____.",
-    //             answers: {
-    //                 a: "Architrave",
-    //                 b: "Tympanum",
-    //                 c: "Entablature",
-    //                 d: "Cornice"
-    //             },
-    //             correctAnswer: "d"
-    //         },
-    //         {
-    //             question: "Item B is called _____.",
-    //             answers: {
-    //                 a: "Architrave",
-    //                 b: "Tympanum",
-    //                 c: "Entablature",
-    //                 d: "Cornice"
-    //             },
-    //             correctAnswer: "a"
-    //         },
-    //         {
-    //             question: "Item C is called _____.",
-    //             answers: {
-    //                 a: "Shaft",
-    //                 b: "Tympanum",
-    //                 c: "Entablature",
-    //                 d: "Cornice"
-    //             },
-    //             correctAnswer: "a"
-    //         },
-    //         {
-    //             question: "Item D is called _____.",
-    //             answers: {
-    //                 a: "Shaft",
-    //                 b: "Tympanum",
-    //                 c: "Entablature",
-    //                 d: "Cornice"
-    //             },
-    //             correctAnswer: "b"
-    //         },
-    //         {
-    //             question: "Item E is called _____.",
-    //             answers: {
-    //                 a: "Shaft",
-    //                 b: "Tympanum",
-    //                 c: "Entablature",
-    //                 d: "Cornice"
-    //             },
-    //             correctAnswer: "c"
-    //         }
-    //     ]
-    // }
+    {
+        image: "image2.PNG",
+        questions: [
+            {
+                question: "Item A is called _____.",
+                answers: {
+                    a: "Architrave",
+                    b: "Tympanum",
+                    c: "Entablature",
+                    d: "Cornice"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "Item B is called _____.",
+                answers: {
+                    a: "Architrave",
+                    b: "Tympanum",
+                    c: "Entablature",
+                    d: "Cornice"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item C is called _____.",
+                answers: {
+                    a: "Shaft",
+                    b: "Tympanum",
+                    c: "Entablature",
+                    d: "Cornice"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item D is called _____.",
+                answers: {
+                    a: "Shaft",
+                    b: "Tympanum",
+                    c: "Entablature",
+                    d: "Cornice"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "Item E is called _____.",
+                answers: {
+                    a: "Shaft",
+                    b: "Tympanum",
+                    c: "Entablature",
+                    d: "Cornice"
+                },
+                correctAnswer: "c"
+            }
+        ]
+    }
 ];
