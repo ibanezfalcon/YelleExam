9894021c55f0877cbeb6bc77d3e84d2ff4fb3622
ibanezfalconf <ibanezfalconf@gmail.com>
2024-06-07 01:43:04 +0800

    changes12 commit

--- renderer/js/questions2.js
+++ renderer/js/questions2.js
@@ -108,5 +108,401 @@ const questions2 = [
                 correctAnswer: "c"
             }
         ]
+    },
+
+    {
+        image: "image3.PNG",
+        questions: [
+            {
+                question: "What is figure A commonly used in garden as stepping stones and floor finishes?",
+                answers: {
+                    a: "Escombro",
+                    b: "Pan de unan stone",
+                    c: "Cobble stone",
+                    d: "Piedra china",
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "What is figure B which is a kind of white coral stone used in residential walls?",
+                answers: {
+                    a: "Yellow slate",
+                    b: "Ibiza slate",
+                    c: "Mactan stone",
+                    d: "Fossil reef stone",
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "What is figure C which is widely known in Negros and popularly used on both floors and walls?",
+                answers: {
+                    a: "Green slates",
+                    b: "Araal slate",
+                    c: "Coral stone",
+                    d: "Onyx stone",
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "What is figure D which is available in variety of sizes, shapes and color commonly used in accent walls?",
+                answers: {
+                    a: "Adobe stone",
+                    b: "Travertine",
+                    c: "Woodstone",
+                    d: "Onyx",
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "What is figure E which is widely available and made from organic materials?",
+                answers: {
+                    a: "Cob block",
+                    b: "Adobe",
+                    c: "Banco block",
+                    d: "Mudbrick",
+                },
+                correctAnswer: "b"
+            }
+        ]
+    },
+
+
+    {
+        image: "image4.PNG",
+        questions: [
+            {
+                question: "What is the spatial organization of figure A?",
+                answers: {
+                    a: "Clustered",
+                    b: "Linear",
+                    c: "Radial",
+                    d: "Centralized"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "What is the spatial organization of figure B?",
+                answers: {
+                    a: "Clustered",
+                    b: "Linear",
+                    c: "Radial",
+                    d: "Centralized"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "What is the spatial organization of figure C?",
+                answers: {
+                    a: "Clustered",
+                    b: "Linear",
+                    c: "Radial",
+                    d: "Centralized"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "What is the spatial organization of figure D?",
+                answers: {
+                    a: "Clustered",
+                    b: "Linear",
+                    c: "Radial",
+                    d: "Grid"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "What is the spatial organization of figure E?",
+                answers: {
+                    a: "Clustered",
+                    b: "Linear",
+                    c: "Radial",
+                    d: "Grid"
+                },
+                correctAnswer: "d"
+            }
+        ]
+    },
+
+    {
+        image: "image5.PNG",
+        questions: [
+            {
+                question: "Item A is called _____.",
+                answers: {
+                    a: "Anchor bolt",
+                    b: "Column cap",
+                    c: "Base plate",
+                    d: "Post base"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "Item B is called _____.",
+                answers: {
+                    a: "Truss hanger",
+                    b: "Angle bracket",
+                    c: "Ledger connector",
+                    d: "Sill plate"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item C is called _____.",
+                answers: {
+                    a: "Drop in anchor",
+                    b: "Wedge anchor",
+                    c: "Sleeve anchor",
+                    d: "Lag shield anchor"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "Item D is called _____.",
+                answers: {
+                    a: "Post cap",
+                    b: "Truss hanger",
+                    c: "Post base",
+                    d: "Joist connector"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item E is used for _____.",
+                answers: {
+                    a: "Glass installations",
+                    b: "Concrete connections",
+                    c: "Wood connections",
+                    d: "Steel installations"
+                },
+                correctAnswer: "a"
+            }
+        ]
+    },
+
+    {
+        image: "image6.PNG",
+        questions: [
+            {
+                question: "Which lighting is required on buildings as per code?",
+                answers: {
+                    a: "Figure D",
+                    b: "Figure C",
+                    c: "Figure E",
+                    d: "Figure A"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "Which lighting has built-in batteries?",
+                answers: {
+                    a: "Figure A",
+                    b: "Figure C",
+                    c: "Figure B",
+                    d: "Figure E"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "Which lighting is suitable for outdoor commercial lighting?",
+                answers: {
+                    a: "Figure C",
+                    b: "Figure D",
+                    c: "Figure A",
+                    d: "Figure A"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "Which lighting fixture is appropriate for safety purposes?",
+                answers: {
+                    a: "Figure C",
+                    b: "Figure E",
+                    c: "Figure A",
+                    d: "Figure D"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "For very high ceiling, which lighting is most efficient?",
+                answers: {
+                    a: "Figure D",
+                    b: "Figure C",
+                    c: "Figure A",
+                    d: "Figure E"
+                },
+                correctAnswer: "d"
+            }
+        ]
+    },
+
+
+    {
+        image: "image7.PNG",
+        questions: [
+            {
+                question: "What is the joinery of figure A?",
+                answers: {
+                    a: "T&G",
+                    b: "Dovetail",
+                    c: "Lap",
+                    d: "Rabbet"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "What is the joinery of figure B?",
+                answers: {
+                    a: "T&G",
+                    b: "Dovetail",
+                    c: "Lap",
+                    d: "Rabbet"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "What is the joinery of figure C?",
+                answers: {
+                    a: "Dado",
+                    b: "Dovetail",
+                    c: "Lap",
+                    d: "Rabbet"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "What is the joinery of figure D?",
+                answers: {
+                    a: "Dado",
+                    b: "Dovetail",
+                    c: "Lap",
+                    d: "Rabbet"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "What is the joinery of figure E?",
+                answers: {
+                    a: "Dado",
+                    b: "Dovetail",
+                    c: "Lap",
+                    d: "Rabbet"
+                },
+                correctAnswer: "b"
+            }
+        ]
+    },
+
+
+    {
+        image: "image8.PNG",
+        questions: [
+            {
+                question: "Item A is called _____.",
+                answers: {
+                    a: "Ball valve",
+                    b: "Gate valve",
+                    c: "Safety valve",
+                    d: "Angle valve"
+                },
+                correctAnswer: "d"
+            },
+            {
+                question: "Item B is called _____.",
+                answers: {
+                    a: "Ball valve",
+                    b: "Gate valve",
+                    c: "Safety valve",
+                    d: "Angle valve"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item C is called _____.",
+                answers: {
+                    a: "Check valve",
+                    b: "Gate valve",
+                    c: "Safety valve",
+                    d: "Angle valve"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "Item D is called _____.",
+                answers: {
+                    a: "Check valve",
+                    b: "Gate valve",
+                    c: "Safety valve",
+                    d: "Angle valve"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "Item E is used for _____.",
+                answers: {
+                    a: "Check valve",
+                    b: "Gate valve",
+                    c: "Safety valve",
+                    d: "Angle valve"
+                },
+                correctAnswer: "c"
+            }
+        ]
+    },
+
+    {
+        image: "image9.PNG",
+        questions: [
+            {
+                question: "What is the construction system of figure A?",
+                answers: {
+                    a: "Diagrid",
+                    b: "EVG 3D",
+                    c: "Thin shell",
+                    d: "Composite"
+                },
+                correctAnswer: "c"
+            },
+            {
+                question: "What is the construction system of figure B?",
+                answers: {
+                    a: "Diagrid",
+                    b: "EVG 3D",
+                    c: "Thin shell",
+                    d: "Composite"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "What is the construction system of figure C?",
+                answers: {
+                    a: "Sandwich panel",
+                    b: "EVG 3D",
+                    c: "Thin shell",
+                    d: "Composite"
+                },
+                correctAnswer: "b"
+            },
+            {
+                question: "What is the construction system of figure D?",
+                answers: {
+                    a: "Sandwich panel",
+                    b: "EVG 3D",
+                    c: "Thin shell",
+                    d: "Composite"
+                },
+                correctAnswer: "a"
+            },
+            {
+                question: "What is the construction system of figure E?",
+                answers: {
+                    a: "Sandwich panel",
+                    b: "EVG 3D",
+                    c: "Thin shell",
+                    d: "Composite"
+                },
+                correctAnswer: "d"
+            }
+        ]
     }
+
 ];
